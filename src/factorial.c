748b0039eb2d94500d56617a44176e5250c315d9
99003758 <78871909+99003758@users.noreply.github.com>
2021-02-24 09:42:43 +0530

    Update factorial.c

--- src/factorial.c
+++ src/factorial.c
@@ -1,1 +1,12 @@
 
+#include<stdio.h>
+int main()
+{
+    int i,fact=1,number=9;
+    for(i=1;i<=number;i++)
+    {
+        fact=fact*i;
+    }
+    printf("factorial of %d is: %d",number,fact);
+    return 0;
+}
