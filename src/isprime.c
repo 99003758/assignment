8769066816035f174daf44962122705ceca4d963
99003758 <78871909+99003758@users.noreply.github.com>
2021-02-24 16:35:36 +0530

    Update isprime.c

--- src/isprime.c
+++ src/isprime.c
@@ -1,1 +1,16 @@
 
+#include "myutils.h"
+void Prime_1(int num)
+{
+    for(int i = 2; i <= num / 2; i++)
+     {
+       if(num % i == 0) 
+       {
+           printf("Number is prime");
+       }
+       else
+       {
+           printf("Number is not prime");
+       }
+     }
+}
