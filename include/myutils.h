71bc91854e206f42dc7512a96797031dc6d6c810
99003758 <78871909+99003758@users.noreply.github.com>
2021-02-24 16:33:00 +0530

    Update myutils.h

--- include/myutils.h
+++ include/myutils.h
@@ -1,1 +1,15 @@
-c
+
+#ifndef __MYUTILS_H
+#define __MYUTILS_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include <stdarg.h> 
+
+void Prime_1(int num);
+int factorial(unsigned int n);
+int isPalindrome(int number);
+int min(int arg_count, ...);
+
+
+#endif
