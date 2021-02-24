ade401143270000fd839498365bb7e4762d7a6fc
99003758 <78871909+99003758@users.noreply.github.com>
2021-02-24 09:44:23 +0530

    Update mystrcat.c

--- src/mystrcat.c
+++ src/mystrcat.c
@@ -1,1 +1,16 @@
 
+
+#include "mystring.h"
+
+char* mystrcat(char *str1, const char *str2);
+{
+  int len,i;
+  len = strlen(str1);
+  for(i=0;str2[i]!='\0';i++)
+  {
+      str[len]=str2[i];
+      len++;
+  }
+  str[len]='\0';
+  return str1;
+}
